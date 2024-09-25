c3eefaa2cc25cef12b0a681242ff11b8a1a77d29
bayena <1001051099@tzfonet.org.il>
2024-09-25 19:22:47 +0300

    ftfytg

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.activity1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.activity1"
--- app/src/main/java/com/example/activity1/MainActivity.java
+++ app/src/main/java/com/example/activity1/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.activity1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView image;
+    private float x,y;
+    Thread thread;
+    TextView textView;
+    int level=1000,counter=0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image=findViewById(R.id.emojy);
+        textView=findViewById(R.id.text1);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                textView.setText(counter+"");
+
+            }
+        });
+
+    }
+    public void startp(View view){
+        thread=new Thread(()->{
+            try {
+                while (true){
+                    startMoveImage();
+                    Thread.sleep(level);
+                }
+            }
+            catch (Exception e){
+
+            }
+        });
+        thread.start();
+    }
+
+    private void startMoveImage() {
+        x=(float) Math.random()*800;
+        y=(float) Math.random()*1600;
+        image.setX(x);
+        image.setY(y);
+
     }
 }
