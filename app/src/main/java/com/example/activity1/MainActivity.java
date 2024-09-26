672f3947f4a6796427ba28fe11b8a82b467b21b0
bayena <1001051099@tzfonet.org.il>
2024-09-26 20:19:54 +0300

    ftfytg

--- app/src/main/java/com/example/activity1/MainActivity.java
+++ app/src/main/java/com/example/activity1/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.activity1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,19 +16,50 @@ public class MainActivity extends AppCompatActivity {
     private float x,y;
     Thread thread;
     TextView textView;
-    int level=1000,counter=0;
+    int level=3000,counter=0,wichlevel=0;
+    RadioButton radioe,radioa,radiod;
+    Button button;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         image=findViewById(R.id.emojy);
         textView=findViewById(R.id.text1);
+        radioe=findViewById(R.id.radioe);
+        radioa=findViewById(R.id.radioa);
+        radiod=findViewById(R.id.radiod);
+        button=findViewById(R.id.stop);
+
+
+        radioe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wichlevel=1;
+                level=2000;
+            }
+        });
+        radioa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wichlevel=2;
+                level=1000;
+            }
+        });
+        radiod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                wichlevel=3;
+                level=600;
+            }
+        });
+
         image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                counter++;
+                counter=counter+wichlevel;
                 textView.setText(counter+"");
 
             }
@@ -44,6 +79,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         thread.start();
+    }
+    public void stopp(View view){
+        thread.interrupt();
+        AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Total Points")
+                .setMessage(String.valueOf(counter))
+                .setPositiveButton("OK", null)
+                .create();
+        dialog.show();
+        counter=0;
+        textView.setText(0+"");
+
+
     }
 
     private void startMoveImage() {
